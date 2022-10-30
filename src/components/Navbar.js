7abb21e444fb5f0fd0f7f69fdc5fdd60051bd3f4
Russell Fraze <russell.fraze@gmail.com>
2022-10-29 19:55:00 -0700

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,75 @@
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { getAuth } from 'firebase/auth'
 
 function Navbar() {
+    const [navIsOpen, setNavIsOpen] = useState(false)
+
     const navigate = useNavigate()
+    const location = useLocation()
+
+    const auth = getAuth()
+
+
+    const pathMatchRoute = (route) => {
+        if (route === location.pathname) {
+            return true
+        }
+    }
+    console.log(navIsOpen)
 
     return (
-        <div>
-            <div className='navbar sticky'>
+        <nav className='navigation'>
 
-                <ul className='nav-links'>
-                    <li onClick={() => navigate('/')}>
-                        <p>Home</p>
-                    </li>
-                    <li onClick={() => navigate('/register')}>
-                        <p>Register</p>
-                    </li>
-                    <li onClick={() => navigate('/sign-in')}>
-                        <p>Sign in</p>
-                    </li>
-                    <li onClick={() => navigate('/recipe-book')}>
-                        <p>Recipe Book</p>
-                    </li>
-                    <li onClick={() => navigate('/shopping-list')}>
-                        <p>Shopping list</p>
-                    </li>
-                    <li onClick={() => navigate('/profile')}>
-                        <p>Profile</p>
-                    </li>
-                </ul>
-
+            <div className='logo'>
+                <h1>BIGSHOP</h1>
             </div>
-        </div>
+
+            <div className='burger' onClick={() => setNavIsOpen(!navIsOpen)}>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+
+            <div className={navIsOpen ? 'navigation-menu expanded' : 'navigation-menu'}>
+                <ul>
+
+                    <li onClick={() => navigate('/')}>
+                        <p className={pathMatchRoute('/') ? 'navbar-active' : ''} >Home</p>
+                    </li>
+
+
+                    {!auth.currentUser &&
+                        <li onClick={() => navigate('/register')}>
+                            <p className={pathMatchRoute('/register') ? 'navbar-active' : ''}>Register</p>
+                        </li>}
+
+                    {!auth.currentUser &&
+                        <li onClick={() => navigate('/sign-in')}>
+                            <p className={pathMatchRoute('/sign-in') ? 'navbar-active' : ''}>Sign in</p>
+                        </li>}
+
+                    {auth.currentUser &&
+                        <li onClick={() => navigate('/recipe-book')}>
+                            <p className={pathMatchRoute('/recipe-book') ? 'navbar-active' : ''}>Recipe Book</p>
+                        </li>}
+
+                    {auth.currentUser &&
+                        <li onClick={() => navigate('/shopping-list')}>
+                            <p className={pathMatchRoute('/shopping-list') ? 'navbar-active' : ''}>Shopping list</p>
+                        </li>}
+
+
+                    <li onClick={() => navigate('/profile')}>
+                        <p className={pathMatchRoute('/profile') ? 'navbar-active' : ''}>Profile</p>
+                    </li>
+
+                </ul>
+            </div>
+
+
+
+        </nav>
     )
 }
 
